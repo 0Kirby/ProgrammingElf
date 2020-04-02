e8437d0a70ebdc908b1fc765420dce897786b710
0Kirby <jty.1234567890@163.com>
2020-04-02 22:37:15 +0800

    support java runtime

---
+++ cloudfunctions/glot/index.js
@@ -0,0 +1,25 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+const got = require('got'); //引用 got
+cloud.init({
+  env: cloud.DYNAMIC_CURRENT_ENV,
+})
+// 云函数入口函数
+exports.main = async (event, context) => {
+  let postResponse = await got('https://run.glot.io/languages/' + event.language + '/latest', {
+    method: 'POST', //post请求
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Token 698fdc18-be4c-4eed-acd1-578d8a46813f'
+    },
+    body: JSON.stringify({ //把json数据（对象）解析成字符串
+      files: [{
+        "name": "Main.java",
+        "content": event.content
+      }],
+      stdin: "",
+      command: ""
+    })
+  })
+  return postResponse.body //返回数据
+}
---
+++ miniprogram/pages/exercise/exercise.js
@@ -0,0 +1,87 @@
+// pages/exercise/exercise.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    languages: ['Java', 'C', 'Python'],
+    index: 0,
+    setLang: "java"
+  },
+
+  bindPickerChange: function (e) {
+    this.setData({
+      index: e.detail.value,
+      setLang: this.data.languages[e.detail.value].toLowerCase()
+    })
+  },
+
+  bindFormSubmit: function (e) {
+    wx.cloud.callFunction({ //调用云函数
+      name: 'glot', //云函数名为http
+      data: {
+        language: this.data.setLang,
+        content: e.detail.value.textarea
+      },
+    }).then(res => { //Promise
+      console.log(JSON.parse(res.result))
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})

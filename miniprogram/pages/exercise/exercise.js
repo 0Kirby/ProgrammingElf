9188ee3963ceae0379b148b03e56e946ed76aba7
0Kirby <jty.1234567890@163.com>
2020-04-03 12:07:08 +0800

    support displaying result in textarea;optimize UI

--- miniprogram/pages/exercise/exercise.js
+++ miniprogram/pages/exercise/exercise.js
@@ -7,15 +7,24 @@ Page({
   data: {
     languages: ['Java', 'C', 'Python'],
     index: 0,
-    setLang: "java",
-    filename: "Main.java",
-    stdin: ""
+    setLang: 'java',
+    filenames: ['Main.java', 'main.c', 'main.py'],
+    filename: '',
+    codes: ['public class Main {\n    public static void main(String[] args) {\n        System.out.println("Java版Hello World!");\n    }\n}',
+      '#include <stdio.h>\n\n  int main() {\n      printf("C版Hello World!\\n");\n      return 0;\n  }',
+      'print("Python版Hello World!")'
+    ],
+    code: '',
+    result: ''
   },
 
   bindPickerChange: function (e) {
     this.setData({
       index: e.detail.value,
-      setLang: this.data.languages[e.detail.value].toLowerCase()
+      setLang: this.data.languages[e.detail.value].toLowerCase(),
+      filename: this.data.filenames[e.detail.value],
+      code: this.data.codes[e.detail.value],
+      result: ''
     })
   },
 
@@ -29,7 +38,22 @@ Page({
         content: e.detail.value.textarea
       },
     }).then(res => { //Promise
-      console.log(JSON.parse(res.result))
+      this.setData({
+        result: JSON.parse(res.result).stdout + JSON.parse(res.result).stderr +
+          JSON.parse(res.result).error
+      })
+    })
+  },
+
+  bindFormReset: function () {
+    this.setData({
+      index: 0,
+      setLang: '',
+      filename: '',
+      stdin: '',
+      content: '',
+      code: '',
+      result: ''
     })
   },
 
@@ -37,7 +61,10 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.setData({
+      filename: this.data.filenames[0],
+      code: this.data.codes[0]
+    })
   },
 
   /**
